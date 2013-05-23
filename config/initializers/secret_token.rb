ab5643c1868f4a5da1e17ef71cd40abd634baeb5
Asbjoern <r3missus@gmail.com>
2013-05-23 22:21:12 +0200

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galorelive::Application.config.secret_token = 'f6b19f1d5b4a4efc82e2fdb785c0d31f1c4be8deb61c5ff2ec4b8503db37548d339d661ce23d1510db85244b2333191022ed13ed7e7354d607116943dda6d49e'
